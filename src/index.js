853bbfade00de92d59a90a90c56e15593dc433df
Usman Tasayev <tasayev.usman@gmail.com>
2021-02-06 23:08:22 -0500

    added basic title and nav buttons home, menu, about, and contact

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('hey')
+const header = document.querySelector('#Header');
+
+const title = document.createElement('h1');
+title.innerHTML = 'Pizza';
+
+
+const nav = document.createElement('div');
+
+const home = document.createElement('button');
+home.innerHTML = 'Home';
+const menu = document.createElement('button');
+menu.innerHTML = 'Menu';
+const about = document.createElement('button');
+about.innerHTML = 'About';
+const contact = document.createElement('button');
+contact.innerHTML = 'Contact';
+
+header.append(title, nav);
+nav.append(home, menu, about, contact);
+
+
